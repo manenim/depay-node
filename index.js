a5d01ee1bcd63aadb104500686aa4f8898d7b247
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2024-01-12 09:59:21 +0100

    new config settings

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.post('/depay/configuration', async(req, res)=>{
 //     "fix": req.body.quantity || 100
 //   },
     accept: [
-      { blockchain: 'polygon', token: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', receiver: '0xA04FED1cab1008F7cA4E83D43B52cd561a10db57', amount: 0.000001 }
+      { blockchain: 'polygon', token: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', receiver: '0xA04FED1cab1008F7cA4E83D43B52cd561a10db57', amount: price*req.body.quantity || 0.000001 }
     ]
   };
 
